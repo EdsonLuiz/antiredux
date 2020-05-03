7c7d8afaea699a40495c1f3905627f2cd978d07c
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-05-03 19:29:16 -0300

    Create global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
+const Global = createGlobalStyle`
+${reset};
+body{
+    background-color:#ecf0f1;
+}
+`;
 
 function App() {
   return (
     <>
+      <Global />
       <h1>Hello</h1>
     </>
   );
